7b2a2bbe0011775edd02ce771203aa2db25d79cd
Augusto <126403151+seocodes@users.noreply.github.com>
2025-07-26 23:02:39 -0300

    Update firebaseConfig.js

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,22 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDIyFHLw8eY8cmw1X_io77HJxGORt7sB3c",
-//     authDomain: "nutrixp-1.firebaseapp.com",
-//     projectId: "nutrixp-1",
-//     storageBucket: "nutrixp-1.firebasestorage.app",
-//     messagingSenderId: "925545468865",
-//     appId: "1:925545468865:web:fdaacf46ab09c3c3395540"
-//   };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = getFirestore(app); 
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -24,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIyFHLw8eY8cmw1X_io77HJxGORt7sB3c",
-  authDomain: "nutrixp-1.firebaseapp.com",
-  projectId: "nutrixp-1",
-  storageBucket: "nutrixp-1.firebasestorage.app",
-  messagingSenderId: "925545468865",
-  appId: "1:925545468865:web:fdaacf46ab09c3c3395540"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
